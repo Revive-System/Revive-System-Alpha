93ad2f44a80de174123921bab550384a73dc5f69
Kex <kexanone@gmail.com>
2023-05-27 04:20:52 +0200

    Handle revive in bandage effect and do it only if all bleedings are stopped

---
+++ Scripts/Game/Components/Gadgets/modded/SCR_ConsumableBandage.c
@@ -0,0 +1,28 @@
+[BaseContainerProps()]
+modded class SCR_ConsumableBandage : SCR_ConsumableEffectHealthItems
+{
+	// Make bandages revive if all bleedings are stopped
+	override void ApplyEffect(notnull IEntity target, notnull IEntity user, IEntity item, SCR_ConsumableEffectAnimationParameters animParams)
+	{
+		super.ApplyEffect(target, user, item, animParams);
+		
+        ChimeraCharacter targetCharacter = ChimeraCharacter.Cast(target);
+        if (!targetCharacter)
+            return;
+			
+		SCR_CharacterControllerComponent targetController = SCR_CharacterControllerComponent.Cast(targetCharacter.GetCharacterController());
+		if (!targetController)
+			return;
+		
+		SCR_CharacterDamageManagerComponent targetDamageMan = SCR_CharacterDamageManagerComponent.Cast(targetCharacter.GetDamageManager());
+		if (!targetDamageMan)
+			return;
+		
+		// Unit gets revived if no bleeding wound is present
+		if (targetController.IsUnconscious() && !targetDamageMan.IsDamagedOverTime(EDamageType.BLEEDING))
+		{
+			targetDamageMan.FullHeal();
+			targetController.SetUnconscious(false);
+		};
+	};
+};

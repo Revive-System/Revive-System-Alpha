7308602e2222d1ba2bb7db917e0328d26d48cb80
Kex <kexanone@gmail.com>
2023-05-28 17:47:55 +0200

    Change mission header parameters
    - Remove m_fUnconsciousBleedoutDuration
    - Add bleeding and regeneration parameters
    - Lower default bleeding rate to 0.56

--- Scripts/Game/Mission/modded/SCR_MissionHeader.c
+++ Scripts/Game/Mission/modded/SCR_MissionHeader.c
@@ -1,5 +1,8 @@
 modded class SCR_MissionHeader : MissionHeader
 {
-	[Attribute(defvalue: "300", uiwidget: UIWidgets.EditBox, desc: "How long it takes in seconds for an unconscious unit to bleed out")]
-	float m_fUnconsciousBleedoutDuration;
+	[Attribute(defvalue: "0.56", uiwidget: UIWidgets.EditBox, desc: "A global bleeding rate multiplier. The higher the value, the faster entities will bleed to death.")]
+	float m_fBleedingRate;
+	
+	[Attribute(defvalue: "1", uiwidget: UIWidgets.EditBox, desc: "A global multiplier affecting how fast entities will recover health and blood levels. The higher the value, the faster the regeneration.")]
+	float m_fHealthAndBloodRegenerationRate;
 };

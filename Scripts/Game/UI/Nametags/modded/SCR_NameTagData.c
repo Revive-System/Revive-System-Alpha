c3861e33b3b9db7eca81855ddbfc665e68e3b579
Kex <kexanone@gmail.com>
2023-05-29 17:31:12 +0200

    Add incapacitated icon to nametag

---
+++ Scripts/Game/UI/Nametags/modded/SCR_NameTagData.c
@@ -0,0 +1,16 @@
+modded class SCR_NameTagData : Managed
+{
+	protected Widget m_wOverlayIcon;
+	
+	void SCR_NameTagData(ResourceName layout, Widget rootWidget)
+	{
+		m_wOverlayIcon = m_NameTagWidget.FindAnyWidget("OverlayIcon");
+	};
+	
+	// -- TO DO: Find a better place to do this
+	override void UpdateTagPos()
+	{
+		super.UpdateTagPos();
+		m_wOverlayIcon.SetVisible(m_CharController.IsUnconscious());
+	};
+};
